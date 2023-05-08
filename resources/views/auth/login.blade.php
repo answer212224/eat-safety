9ebd4ae82d77df7f02deff6adb05fb70a3428a11
Liam Li <liam.li@eatogether.com.tw>
2023-05-08 18:02:22 +0800

    登入

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,126 +2,95 @@
 
     <x-slot:pageTitle>
         {{-- {{$title}}  --}}
-    </x-slot>
+        </x-slot>
 
-    <!-- BEGIN GLOBAL MANDATORY STYLES -->
-    <x-slot:headerFiles>
-        <!--  BEGIN CUSTOM STYLE FILE  -->
-        @vite(['resources/scss/light/assets/authentication/auth-boxed.scss'])
-        @vite(['resources/scss/dark/assets/authentication/auth-boxed.scss'])
+        <!-- BEGIN GLOBAL MANDATORY STYLES -->
+        <x-slot:headerFiles>
+            <!--  BEGIN CUSTOM STYLE FILE  -->
+            @vite(['resources/scss/light/assets/authentication/auth-boxed.scss'])
+            @vite(['resources/scss/dark/assets/authentication/auth-boxed.scss'])
 
-        <style>
-            #load_screen {
-                display: none;
-            }
-        </style>
-        <!--  END CUSTOM STYLE FILE  -->
-    </x-slot>
-    <!-- END GLOBAL MANDATORY STYLES -->
-    
-    <div class="auth-container d-flex">
+            <style>
+                #load_screen {
+                    display: none;
+                }
+            </style>
+            <!--  END CUSTOM STYLE FILE  -->
+            </x-slot>
+            <!-- END GLOBAL MANDATORY STYLES -->
 
-        <div class="container mx-auto align-self-center">
-    
-            <div class="row">
-    
-                <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-8 col-12 d-flex flex-column align-self-center mx-auto">
-                    <div class="card mt-3 mb-3">
-                        <div class="card-body">
-    
-                            <div class="row">
-                                <div class="col-md-12 mb-3">
-                                    
-                                    <h2>Sign In</h2>
-                                    <p>Enter your email and password to login</p>
-                                    
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="mb-3">
-                                        <label class="form-label">Email</label>
-                                        <input type="email" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="mb-4">
-                                        <label class="form-label">Password</label>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="mb-3">
-                                        <div class="form-check form-check-primary form-check-inline">
-                                            <input class="form-check-input me-3" type="checkbox" id="form-check-default">
-                                            <label class="form-check-label" for="form-check-default">
-                                                Remember me
-                                            </label>
+            <div class="auth-container d-flex">
+
+                <div class="container mx-auto align-self-center">
+
+                    <div class="row">
+
+                        <div
+                            class="col-xxl-4 col-xl-5 col-lg-5 col-md-8 col-12 d-flex flex-column align-self-center mx-auto">
+                            <div class="card mt-3 mb-3">
+                                <div class="card-body">
+
+                                    <div class="row">
+                                        <div class="col-md-12 mb-3">
+
+                                            <h2>Sign In</h2>
+                                            <p>Enter your email and password to login</p>
+
                                         </div>
-                                    </div>
-                                </div>
-                                
-                                <div class="col-12">
-                                    <div class="mb-4">
-                                        <button class="btn btn-secondary w-100">SIGN IN</button>
-                                    </div>
-                                </div>
-                                
-                                <div class="col-12 mb-4">
-                                    <div class="">
-                                        <div class="seperator">
-                                            <hr>
-                                            <div class="seperator-text"> <span>Or continue with</span></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                
-                                <div class="col-sm-4 col-12">
-                                    <div class="mb-4">
-                                        <button class="btn  btn-social-login w-100 ">
-                                            <img src="{{Vite::asset('resources/images/google-gmail.svg')}}" alt="" class="img-fluid">
-                                            <span class="btn-text-inner">Google</span>
-                                        </button>
-                                    </div>
-                                </div>
-    
-                                <div class="col-sm-4 col-12">
-                                    <div class="mb-4">
-                                        <button class="btn  btn-social-login w-100">
-                                            <img src="{{Vite::asset('resources/images/github-icon.svg')}}" alt="" class="img-fluid">
-                                            <span class="btn-text-inner">Github</span>
-                                        </button>
-                                    </div>
-                                </div>
-    
-                                <div class="col-sm-4 col-12">
-                                    <div class="mb-4">
-                                        <button class="btn  btn-social-login w-100">
-                                            <img src="{{Vite::asset('resources/images/twitter.svg')}}" alt="" class="img-fluid">
-                                            <span class="btn-text-inner">Twitter</span>
-                                        </button>
-                                    </div>
-                                </div>
+                                        <form action="{{ route('login') }}" method="post">
+                                            <div class="col-md-12">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Email</label>
+                                                    <input type="email" class="form-control" name="email">
+                                                </div>
+                                            </div>
+                                            <div class="col-12">
+                                                <div class="mb-4">
+                                                    <label class="form-label">Password</label>
+                                                    <input type="text" class="form-control" name="password">
+                                                </div>
+                                            </div>
+                                            <div class="col-12">
+                                                <div class="mb-3">
+                                                    <div class="form-check form-check-primary form-check-inline">
+                                                        <input class="form-check-input me-3" type="checkbox"
+                                                            id="form-check-default">
+                                                        <label class="form-check-label" for="form-check-default">
+                                                            Remember me
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                            </div>
 
-                                <div class="col-12">
-                                    <div class="text-center">
-                                        <p class="mb-0">Dont't have an account ? <a href="javascript:void(0);" class="text-warning">Sign Up</a></p>
+                                            @csrf
+                                            <div class="col-12">
+                                                <div class="mb-4">
+                                                    <button class="btn btn-secondary w-100">SIGN IN</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                        <div class="col-12">
+                                            <div class="text-center">
+                                                <p class="mb-0">Dont't have an account ? <a href="javascript:void(0);"
+                                                        class="text-warning">Sign Up</a></p>
+                                            </div>
+                                        </div>
+
                                     </div>
+
                                 </div>
-                                
                             </div>
-                            
                         </div>
+
                     </div>
+
                 </div>
-                
+
             </div>
-            
-        </div>
 
-    </div>
-    
-    <!--  BEGIN CUSTOM SCRIPTS FILE  -->
-    <x-slot:footerFiles>
+            <!--  BEGIN CUSTOM SCRIPTS FILE  -->
+            <x-slot:footerFiles>
 
-    </x-slot>
-    <!--  END CUSTOM SCRIPTS FILE  -->
+                </x-slot>
+                <!--  END CUSTOM SCRIPTS FILE  -->
 </x-base-layout>
