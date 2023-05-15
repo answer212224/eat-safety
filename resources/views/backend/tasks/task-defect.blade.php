6600c9329613652e604b869effa91ef214cc37d4
Liam Li <liam.li@eatogether.com.tw>
2023-05-15 10:35:11 +0800

    主管核對清單

--- resources/views/backend/tasks/task-defect.blade.php
+++ resources/views/backend/tasks/task-defect.blade.php
@@ -14,36 +14,23 @@
 
 
     <div class="row layout-top-spacing">
-        <div class="col-lg-3 col-md-3 col-sm-3 mb-4">
-            <input id="t-text" type="text" name="txt" placeholder="Search" class="form-control" required="">
-        </div>
-        <div class="col-xl-2 col-lg-3 col-md-3 col-sm-3 mb-4 ms-auto">
-            <select class="form-select form-select" aria-label="Default select example">
-                <option selected="">All Category</option>
-                <option value="3">Wordpress</option>
-                <option value="1">Admin</option>
-                <option value="2">Themeforest</option>
-                <option value="3">Travel</option>
-            </select>
-        </div>
 
-        <div class="col-xl-2 col-lg-3 col-md-3 col-sm-3 mb-4">
-            <select class="form-select form-select" aria-label="Default select example">
-                <option selected="">Sort By</option>
-                <option value="1">Recent</option>
-                <option value="2">Most Upvoted</option>
-                <option value="3">Popular</option>
-            </select>
-        </div>
     </div>
 
     <div class="row">
         @foreach ($task->taskHasDefects as $taskHasDefect)
-            <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6 mb-4">
-                <a href="./app-blog-post.html" class="card style-2 mb-md-0 mb-4">
-                    <img src="{{ asset('storage/' . $taskHasDefect->images[0]) }}" class="card-img-top" alt="...">
+            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mb-12">
+                <div class="card style-2 mb-4">
+                    @foreach ($taskHasDefect->images as $image)
+                        <a href="{{ asset('storage/' . $image) }}"><img src="{{ asset('storage/' . $image) }}"
+                                class="card-img-top my-1" alt="..."></a>
+                    @endforeach
+
+
                     <div class="card-body px-0 pb-0">
-                        <h5 class="card-title mb-3">14 Tips to improve your photography</h5>
+                        <h5 class="card-title mb-3">{{ $taskHasDefect->defect->category }}</h5>
+                        <h6>{{ $taskHasDefect->defect->title }}</h6>
+                        <p>{{ $taskHasDefect->defect->description }}</p>
                         <div class="media mt-4 mb-0 pt-1">
                             {{-- <img src="" class="card-media-image me-3" alt=""> --}}
                             <div class="media-body">
@@ -52,7 +39,8 @@
                             </div>
                         </div>
                     </div>
-                </a>
+
+                </div>
             </div>
         @endforeach
 
