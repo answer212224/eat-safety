f671040f4a6bdf8a5c283f32128585f2d8e3eb72
Liam Li <liam.li@eatogether.com.tw>
2023-06-01 15:33:02 +0800

    nav 更改

--- resources/views/components/navbar/style-vertical-menu.blade.php
+++ resources/views/components/navbar/style-vertical-menu.blade.php
@@ -68,8 +68,8 @@
                     </svg>
                 </a>
             </li>
-
-            <li class="nav-item dropdown notification-dropdown">
+            {{-- notification --}}
+            {{-- <li class="nav-item dropdown notification-dropdown">
                 <a href="javascript:void(0);" class="nav-link dropdown-toggle" id="notificationDropdown"
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
@@ -266,7 +266,8 @@
                     </div>
                 </div>
 
-            </li>
+            </li> --}}
+            {{-- end notification --}}
 
             <li class="nav-item dropdown user-profile-dropdown  order-lg-0 order-1">
                 <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown"
@@ -291,7 +292,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="dropdown-item">
+                    {{-- <div class="dropdown-item">
                         <a href="user-profile.html">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                 viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
@@ -300,30 +301,8 @@
                                 <circle cx="12" cy="7" r="4"></circle>
                             </svg> <span>Profile</span>
                         </a>
-                    </div>
-                    <div class="dropdown-item">
-                        <a href="app-mailbox.html">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                stroke-linecap="round" stroke-linejoin="round" class="feather feather-inbox">
-                                <polyline points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline>
-                                <path
-                                    d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z">
-                                </path>
-                            </svg> <span>Inbox</span>
-                        </a>
-                    </div>
-                    <div class="dropdown-item">
-                        <a href="auth-boxed-lockscreen.html">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock">
-                                <rect x="3" y="11" width="18" height="11" rx="2"
-                                    ry="2"></rect>
-                                <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
-                            </svg> <span>Lock Screen</span>
-                        </a>
-                    </div>
+                    </div> --}}
+
                     <div class="dropdown-item">
                         <form action="{{ route('logout') }}" method="post">
                             @csrf
@@ -334,7 +313,7 @@
                                     <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
                                     <polyline points="16 17 21 12 16 7"></polyline>
                                     <line x1="21" y1="12" x2="9" y2="12"></line>
-                                </svg> <span>Log Out</span>
+                                </svg> <span>登出</span>
                             </button>
                         </form>
                     </div>
