43c9a53ab31456f8386506e3a9af6293cea12d9a
Liam Li <liam.li@eatogether.com.tw>
2023-06-28 16:54:36 +0800

    待確認

--- app/Models/Defect.php
+++ app/Models/Defect.php
@@ -29,6 +29,6 @@ class Defect extends Model
 
     public static function getDescriptionWhereByGroupAndTitle($group, $title, Carbon $latestDefect)
     {
-        return self::whereYear('effective_date', $latestDefect)->whereMonth('effective_date', $latestDefect)->where('group', $group)->where('title', $title)->orWhere('group', '待確認')->get();
+        return self::whereYear('effective_date', $latestDefect)->whereMonth('effective_date', $latestDefect)->where('group', $group)->where('title', $title);
     }
 }
