a434981a8f8e594fa5d3649e78dbe260219c7756
Liam Li <liam.li@eatogether.com.tw>
2023-08-30 15:49:13 +0800

    版面優化

--- resources/views/backend/restaurants/chart.blade.php
+++ resources/views/backend/restaurants/chart.blade.php
@@ -2,88 +2,95 @@
 
     <x-slot:pageTitle>
         {{ $title }}
-        </x-slot>
+    </x-slot>
 
-        <!-- BEGIN GLOBAL MANDATORY STYLES -->
-        <x-slot:headerFiles>
-            <!--  BEGIN CUSTOM STYLE FILE  -->
-            <link rel="stylesheet" href="{{ asset('plugins/apex/apexcharts.css') }}">
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <x-slot:headerFiles>
+        <!--  BEGIN CUSTOM STYLE FILE  -->
+        <link rel="stylesheet" href="{{ asset('plugins/apex/apexcharts.css') }}">
 
-            @vite(['resources/scss/light/assets/components/list-group.scss'])
-            @vite(['resources/scss/light/assets/widgets/modules-widgets.scss'])
+        @vite(['resources/scss/light/assets/components/list-group.scss'])
+        @vite(['resources/scss/light/assets/widgets/modules-widgets.scss'])
 
-            @vite(['resources/scss/dark/assets/components/list-group.scss'])
-            @vite(['resources/scss/dark/assets/widgets/modules-widgets.scss'])
+        @vite(['resources/scss/dark/assets/components/list-group.scss'])
+        @vite(['resources/scss/dark/assets/widgets/modules-widgets.scss'])
 
-            <!--  END CUSTOM STYLE FILE  -->
-            </x-slot>
-            <!-- END GLOBAL MANDATORY STYLES -->
+        <!--  END CUSTOM STYLE FILE  -->
+    </x-slot>
+    <!-- END GLOBAL MANDATORY STYLES -->
 
-            <div class="row layout-top-spacing">
+    <div class="row layout-top-spacing">
 
-                <div class="col-xl-9 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
+        <div class="col-xl-9 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
+            <div class="card">
+                <div class="card-header">
+                    {{ $title }}
+                </div>
+                <div class="card-body">
                     <div id="chart"></div>
                 </div>
-                <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
-                    <livewire:restaurant-activites :restaurant="$restaurant" />
-                </div>
-
-
             </div>
+        </div>
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
+            <livewire:restaurant-activites :restaurant="$restaurant" />
+        </div>
 
-            <!--  BEGIN CUSTOM SCRIPTS FILE  -->
-            <x-slot:footerFiles>
 
-                <script src="{{ asset('plugins/apex/apexcharts.min.js') }}"></script>
+    </div>
 
-                <script>
-                        var options = {
-                            series: [{
-                                name: '食安缺失',
-                                data: @json($defects)
-                            }, {
-                                name: '清檢缺失',
-                                data: @json($clearDefects)
-                            },],
-                            chart: {
-                                type: 'bar',
-                                height: 500
-                            },
-                            plotOptions: {
-                                bar: {
-                                    horizontal: false,
-                                    columnWidth: '55%',
-                                    endingShape: 'rounded'
-                                },
-                            },
-                            dataLabels: {
-                                enabled: false
-                            },
-                            stroke: {
-                                show: true,
-                                width: 2,
-                                colors: ['transparent']
-                            },
-                            xaxis: {
-                                categories: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
-                            },
-                            yaxis: {
-                                title: {
-                                    text: '數量'
-                                }
-                            },
-                            fill: {
-                                opacity: 1
-                            },
-                            tooltip: {
+    <!--  BEGIN CUSTOM SCRIPTS FILE  -->
+    <x-slot:footerFiles>
 
-                            }
-                        };
+        <script src="{{ asset('plugins/apex/apexcharts.min.js') }}"></script>
 
-                    var chart = new ApexCharts(document.querySelector("#chart"), options);
-                    chart.render();
-                </script>
+        <script>
+            var options = {
+                series: [{
+                    name: '食安缺失',
+                    data: @json($defects)
+                }, {
+                    name: '清檢缺失',
+                    data: @json($clearDefects)
+                }, ],
+                chart: {
+                    type: 'bar',
+                    height: 500
+                },
+                plotOptions: {
+                    bar: {
+                        horizontal: false,
+                        columnWidth: '55%',
+                        endingShape: 'rounded'
+                    },
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                stroke: {
+                    show: true,
+                    width: 2,
+                    colors: ['transparent']
+                },
+                xaxis: {
+                    categories: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+                },
+                yaxis: {
+                    title: {
+                        text: '數量'
+                    }
+                },
+                fill: {
+                    opacity: 1
+                },
+                tooltip: {
 
-                </x-slot>
-                <!--  END CUSTOM SCRIPTS FILE  -->
+                }
+            };
+
+            var chart = new ApexCharts(document.querySelector("#chart"), options);
+            chart.render();
+        </script>
+
+    </x-slot>
+    <!--  END CUSTOM SCRIPTS FILE  -->
 </x-base-layout>
