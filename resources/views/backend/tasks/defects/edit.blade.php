a76fe87e4c27546d159b0980bd136a416e71c226
Liam Li <liam.li@eatogether.com.tw>
2023-09-14 10:03:56 +0800

    上一頁路徑修正

--- resources/views/backend/tasks/defects/edit.blade.php
+++ resources/views/backend/tasks/defects/edit.blade.php
@@ -2,152 +2,156 @@
 
     <x-slot:pageTitle>
         {{ $title }}
-        </x-slot>
+    </x-slot>
 
-        <!-- BEGIN GLOBAL MANDATORY STYLES -->
-        <x-slot:headerFiles>
-            <!--  BEGIN CUSTOM STYLE FILE  -->
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <x-slot:headerFiles>
+        <!--  BEGIN CUSTOM STYLE FILE  -->
 
-            <link rel="stylesheet" href="{{ asset('plugins/tagify/tagify.css') }}">
+        <link rel="stylesheet" href="{{ asset('plugins/tagify/tagify.css') }}">
 
-            <link rel="stylesheet" href="{{ asset('plugins/filepond/filepond.min.css') }}">
-            <link rel="stylesheet" href="{{ asset('plugins/filepond/FilePondPluginImagePreview.min.css') }}">
-            @vite(['resources/scss/light/plugins/filepond/custom-filepond.scss'])
-            @vite(['resources/scss/dark/plugins/filepond/custom-filepond.scss'])
+        <link rel="stylesheet" href="{{ asset('plugins/filepond/filepond.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('plugins/filepond/FilePondPluginImagePreview.min.css') }}">
+        @vite(['resources/scss/light/plugins/filepond/custom-filepond.scss'])
+        @vite(['resources/scss/dark/plugins/filepond/custom-filepond.scss'])
 
-            @vite(['resources/scss/light/assets/forms/switches.scss'])
-            @vite(['resources/scss/light/plugins/editors/quill/quill.snow.scss'])
-            @vite(['resources/scss/light/plugins/editors/quill/quill.snow.scss'])
-            @vite(['resources/scss/light/plugins/tagify/custom-tagify.scss'])
-            @vite(['resources/scss/light/assets/apps/blog-create.scss'])
+        @vite(['resources/scss/light/assets/forms/switches.scss'])
+        @vite(['resources/scss/light/plugins/editors/quill/quill.snow.scss'])
+        @vite(['resources/scss/light/plugins/editors/quill/quill.snow.scss'])
+        @vite(['resources/scss/light/plugins/tagify/custom-tagify.scss'])
+        @vite(['resources/scss/light/assets/apps/blog-create.scss'])
 
-            @vite(['resources/scss/dark/assets/forms/switches.scss'])
-            @vite(['resources/scss/dark/plugins/editors/quill/quill.snow.scss'])
-            @vite(['resources/scss/dark/plugins/editors/quill/quill.snow.scss'])
-            @vite(['resources/scss/dark/plugins/tagify/custom-tagify.scss'])
-            @vite(['resources/scss/dark/assets/apps/blog-create.scss'])
+        @vite(['resources/scss/dark/assets/forms/switches.scss'])
+        @vite(['resources/scss/dark/plugins/editors/quill/quill.snow.scss'])
+        @vite(['resources/scss/dark/plugins/editors/quill/quill.snow.scss'])
+        @vite(['resources/scss/dark/plugins/tagify/custom-tagify.scss'])
+        @vite(['resources/scss/dark/assets/apps/blog-create.scss'])
 
-            <link rel="stylesheet" type="text/css" href="{{ asset('plugins/tomSelect/tom-select.default.min.css') }}">
-            @vite(['resources/scss/light/plugins/tomSelect/custom-tomSelect.scss'])
-            @vite(['resources/scss/dark/plugins/tomSelect/custom-tomSelect.scss'])
+        <link rel="stylesheet" type="text/css" href="{{ asset('plugins/tomSelect/tom-select.default.min.css') }}">
+        @vite(['resources/scss/light/plugins/tomSelect/custom-tomSelect.scss'])
+        @vite(['resources/scss/dark/plugins/tomSelect/custom-tomSelect.scss'])
 
-            <link rel="stylesheet" type="text/css"
-                href="{{ asset('plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css') }}">
-            @vite(['resources/scss/light/plugins/bootstrap-touchspin/custom-jquery.bootstrap-touchspin.min.scss'])
-            @vite(['resources/scss/dark/plugins/bootstrap-touchspin/custom-jquery.bootstrap-touchspin.min.scss'])
-            <style>
-                input[type="number"]::-webkit-inner-spin-button,
-                input[type="number"]::-webkit-outer-spin-button {
-                    -webkit-appearance: none;
-                    margin: 0;
-                }
+        <link rel="stylesheet" type="text/css"
+            href="{{ asset('plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css') }}">
+        @vite(['resources/scss/light/plugins/bootstrap-touchspin/custom-jquery.bootstrap-touchspin.min.scss'])
+        @vite(['resources/scss/dark/plugins/bootstrap-touchspin/custom-jquery.bootstrap-touchspin.min.scss'])
+        <style>
+            input[type="number"]::-webkit-inner-spin-button,
+            input[type="number"]::-webkit-outer-spin-button {
+                -webkit-appearance: none;
+                margin: 0;
+            }
 
-                /* 隱藏输入框默认样式 */
-                input[type="number"] {
-                    -moz-appearance: textfield;
-                    /* Firefox */
-                }
-            </style>
-            <!--  END CUSTOM STYLE FILE  -->
-            </x-slot>
-            <!-- END GLOBAL MANDATORY STYLES -->
+            /* 隱藏输入框默认样式 */
+            input[type="number"] {
+                -moz-appearance: textfield;
+                /* Firefox */
+            }
+        </style>
+        <!--  END CUSTOM STYLE FILE  -->
+    </x-slot>
+    <!-- END GLOBAL MANDATORY STYLES -->
 
-            @if ($taskHasDefect->task->category == '清潔檢查')
-                <form action="{{ route('task-clear-defect-update', ['taskHasDefect' => $taskHasDefect]) }}"
-                    method="post">
-                @else
-                    <form action="{{ route('task-defect-update', ['taskHasDefect' => $taskHasDefect]) }}"
-                        method="post">
-            @endif
+    @if ($taskHasDefect->task->category == '清潔檢查')
+        <form action="{{ route('task-clear-defect-update', ['taskHasDefect' => $taskHasDefect]) }}" method="post">
+        @else
+            <form action="{{ route('task-defect-update', ['taskHasDefect' => $taskHasDefect]) }}" method="post">
+    @endif
 
-            @csrf
-            <div class="row mb-4 layout-spacing layout-top-spacing">
+    @csrf
+    <div class="row mb-4 layout-spacing layout-top-spacing">
 
-                <div class="col-xxl-9 col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                    <div class="widget-content widget-content-area blog-create-section mb-4">
-                        <label for="workspace">工作區</label>
-                        <select class="form-select" name="workspace" id="">
-                            @foreach ($taskHasDefect->task->restaurant->restaurantWorkspaces as $restaurantWorkspace)
-                                @if ($taskHasDefect->restaurant_workspace_id == $restaurantWorkspace->id)
-                                    <option value="{{ $restaurantWorkspace->id }}" selected>
-                                        {{ $restaurantWorkspace->area }}</option>
-                                @else
-                                    <option value="{{ $restaurantWorkspace->id }}">
-                                        {{ $restaurantWorkspace->area }}</option>
-                                @endif
-                            @endforeach
-                        </select>
-                    </div>
-
-                    <div class="widget-content widget-content-area blog-create-section">
-                        @if ($taskHasDefect->task->category == '清潔檢查')
-                            <livewire:cleaning-select :taskHasDefect="$taskHasDefect" />
+        <div class="col-xxl-9 col-xl-12 col-lg-12 col-md-12 col-sm-12">
+            <div class="widget-content widget-content-area blog-create-section mb-4">
+                <label for="workspace">工作區</label>
+                <select class="form-select" name="workspace" id="">
+                    @foreach ($taskHasDefect->task->restaurant->restaurantWorkspaces as $restaurantWorkspace)
+                        @if ($taskHasDefect->restaurant_workspace_id == $restaurantWorkspace->id)
+                            <option value="{{ $restaurantWorkspace->id }}" selected>
+                                {{ $restaurantWorkspace->area }}</option>
                         @else
-                            <livewire:defect-select :taskHasDefect="$taskHasDefect" />
+                            <option value="{{ $restaurantWorkspace->id }}">
+                                {{ $restaurantWorkspace->area }}</option>
                         @endif
-                    </div>
-                </div>
+                    @endforeach
+                </select>
+            </div>
 
-                <div class="col-xxl-3 col-xl-12 col-lg-12 col-md-12 col-sm-12 mt-xxl-0 mt-4">
-                    <div class="widget-content widget-content-area blog-create-section">
+            <div class="widget-content widget-content-area blog-create-section">
+                @if ($taskHasDefect->task->category == '清潔檢查')
+                    <livewire:cleaning-select :taskHasDefect="$taskHasDefect" />
+                @else
+                    <livewire:defect-select :taskHasDefect="$taskHasDefect" />
+                @endif
+            </div>
+        </div>
+
+        <div class="col-xxl-3 col-xl-12 col-lg-12 col-md-12 col-sm-12 mt-xxl-0 mt-4">
+            <div class="widget-content widget-content-area blog-create-section">
+                <div class="row">
+
+                    <div class="col-xxl-12 col-md-12 mb-4">
+                        {{-- 缺失照片 --}}
                         <div class="row">
-
-                            <div class="col-xxl-12 col-md-12 mb-4">
-                                {{-- 缺失照片 --}}
-                                <div class="row">
-                                    @foreach($taskHasDefect->images as $image)
-                                        <img src="{{ asset("storage/$image") }}" alt="" class="m-2">
-                                    @endforeach
-                                </div>
-                                
-                            </div>
-
-                            <div class="col-xxl-12 col-sm-4 col-12 mx-auto">
-                                <button class="btn btn-success w-100">更新</button>
-                                <a href="{{ route('task-clear-defect-owner',['task' => $taskHasDefect->task->id]) }}" class="btn btn-dark w-100 my-3">上一頁</a>
-                            </div>
+                            @foreach ($taskHasDefect->images as $image)
+                                <img src="{{ asset("storage/$image") }}" alt="" class="m-2">
+                            @endforeach
                         </div>
+
                     </div>
-                </div>
 
-
-            </div>
-            </form>
-
-            <div class="col-xxl-3 col-xl-12 col-lg-12 col-md-12 col-sm-12 mt-xxl-0 mt-4">
-                <div class="widget-content widget-content-area blog-create-section">
-                    <div class="row">
+                    <div class="col-xxl-12 col-sm-4 col-12 mx-auto">
+                        <button class="btn btn-success w-100">更新</button>
                         @if ($taskHasDefect->task->category == '清潔檢查')
-                            <form action="{{ route('task-clear-defect-delete', ['taskHasDefect' => $taskHasDefect]) }}"
-                                method="post">
-                            @else
-                                <form action="{{ route('task-defect-delete', ['taskHasDefect' => $taskHasDefect]) }}"
-                                    method="post">
+                            <a href="{{ route('task-clear-defect-owner', ['task' => $taskHasDefect->task->id]) }}"
+                                class="btn btn-dark w-100 my-3">上一頁</a>
+                        @else
+                            <a href="{{ route('task-defect-owner', ['task' => $taskHasDefect->task->id]) }}"
+                                class="btn btn-dark w-100 my-3">上一頁</a>
                         @endif
-
-                        @csrf
-                        <div class="col-xxl-12 col-sm-4 col-12 mx-auto">
-                            <button class="btn btn-danger w-100">刪除</button>
-                        </div>
-                        </form>
                     </div>
                 </div>
             </div>
-            <!--  BEGIN CUSTOM SCRIPTS FILE  -->
-            <x-slot:footerFiles>
-                <script src="{{ asset('plugins/editors/quill/quill.js') }}"></script>
-                <script src="{{ asset('plugins/filepond/filepond.min.js') }}"></script>
-                <script src="{{ asset('plugins/filepond/FilePondPluginFileValidateType.min.js') }}"></script>
-                <script src="{{ asset('plugins/filepond/FilePondPluginImageExifOrientation.min.js') }}"></script>
-                <script src="{{ asset('plugins/filepond/FilePondPluginImagePreview.min.js') }}"></script>
-                <script src="{{ asset('plugins/filepond/FilePondPluginImageCrop.min.js') }}"></script>
-                <script src="{{ asset('plugins/filepond/FilePondPluginImageResize.min.js') }}"></script>
-                <script src="{{ asset('plugins/filepond/FilePondPluginImageTransform.min.js') }}"></script>
-                <script src="{{ asset('plugins/filepond/filepondPluginFileValidateSize.min.js') }}"></script>
+        </div>
 
 
-                <script src="{{ asset('plugins/tagify/tagify.min.js') }}"></script>
-                <script src="{{ asset('plugins/filepond/custom-filepond.js') }}?20230823"></script>
-                </x-slot>
-                <!--  END CUSTOM SCRIPTS FILE  -->
+    </div>
+    </form>
+
+    <div class="col-xxl-3 col-xl-12 col-lg-12 col-md-12 col-sm-12 mt-xxl-0 mt-4">
+        <div class="widget-content widget-content-area blog-create-section">
+            <div class="row">
+                @if ($taskHasDefect->task->category == '清潔檢查')
+                    <form action="{{ route('task-clear-defect-delete', ['taskHasDefect' => $taskHasDefect]) }}"
+                        method="post">
+                    @else
+                        <form action="{{ route('task-defect-delete', ['taskHasDefect' => $taskHasDefect]) }}"
+                            method="post">
+                @endif
+
+                @csrf
+                <div class="col-xxl-12 col-sm-4 col-12 mx-auto">
+                    <button class="btn btn-danger w-100">刪除</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!--  BEGIN CUSTOM SCRIPTS FILE  -->
+    <x-slot:footerFiles>
+        <script src="{{ asset('plugins/editors/quill/quill.js') }}"></script>
+        <script src="{{ asset('plugins/filepond/filepond.min.js') }}"></script>
+        <script src="{{ asset('plugins/filepond/FilePondPluginFileValidateType.min.js') }}"></script>
+        <script src="{{ asset('plugins/filepond/FilePondPluginImageExifOrientation.min.js') }}"></script>
+        <script src="{{ asset('plugins/filepond/FilePondPluginImagePreview.min.js') }}"></script>
+        <script src="{{ asset('plugins/filepond/FilePondPluginImageCrop.min.js') }}"></script>
+        <script src="{{ asset('plugins/filepond/FilePondPluginImageResize.min.js') }}"></script>
+        <script src="{{ asset('plugins/filepond/FilePondPluginImageTransform.min.js') }}"></script>
+        <script src="{{ asset('plugins/filepond/filepondPluginFileValidateSize.min.js') }}"></script>
+
+
+        <script src="{{ asset('plugins/tagify/tagify.min.js') }}"></script>
+        <script src="{{ asset('plugins/filepond/custom-filepond.js') }}?20230823"></script>
+    </x-slot>
+    <!--  END CUSTOM SCRIPTS FILE  -->
 </x-base-layout>
