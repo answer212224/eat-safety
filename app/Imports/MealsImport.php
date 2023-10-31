797ec583244c746cb029606ca63824f661ec7543
Liam Li <liam.li@eatogether.com.tw>
2023-10-31 09:30:03 +0800

    Comment out code related to checking for existing audit tasks in MealsImport.php

--- app/Imports/MealsImport.php
+++ app/Imports/MealsImport.php
@@ -41,17 +41,17 @@ class MealsImport implements ToCollection
 
 
         // 檢查採樣是否有該月份的稽核任務
-        $taskHasMeals =  Task::whereHas('meals', function ($query) use ($collection) {
-            $query->whereYear('effective_date', $collection[0]['effective_date'])
-                ->whereMonth('effective_date', $collection[0]['effective_date']);
-        })->get();
+        // $taskHasMeals =  Task::whereHas('meals', function ($query) use ($collection) {
+        //     $query->whereYear('effective_date', $collection[0]['effective_date'])
+        //         ->whereMonth('effective_date', $collection[0]['effective_date']);
+        // })->get();
 
         // 如有該月份的稽核任務，則不可更新該月份的餐點採樣資料
-        if ($taskHasMeals->count() > 0) {
-            throw new \Exception("已有{$collection[0]['effective_date']->format('Y-m')}月的採樣紀錄，無法更新{$collection[0]['effective_date']->format('Y-m')}月份的餐點採樣資料");
-        }
+        // if ($taskHasMeals->count() > 0) {
+        //     throw new \Exception("已有{$collection[0]['effective_date']->format('Y-m')}月的採樣紀錄，無法更新{$collection[0]['effective_date']->format('Y-m')}月份的餐點採樣資料");
+        // }
         // 如無該月份的稽核任務，則可更新該月份的餐點採樣資料
-        Meal::whereYear('effective_date', $collection[0]['effective_date'])->whereMonth('effective_date', $collection[0]['effective_date'])->delete();
+        // Meal::whereYear('effective_date', $collection[0]['effective_date'])->whereMonth('effective_date', $collection[0]['effective_date'])->delete();
 
         // 儲存資料
         foreach ($collection as $item) {
