a303e2341a844b58f34ab9c0e13bb1e903335884
Liam Li <liam.li@eatogether.com.tw>
2023-12-28 16:07:54 +0800

    Add Vue.js app initialization code

--- resources/views/blank.blade.php
+++ resources/views/blank.blade.php
@@ -2,10 +2,31 @@
     <x-slot:pageTitle>
         {{ $title }}
     </x-slot:pageTitle>
-    
+
     <x-slot:headerFiles>
     </x-slot:headerFiles>
 
+    <div id="app">
+        <v-app v-cloak>
+            <v-main class="grey lighten-4">
+                <v-container>
+
+
+
+                </v-container>
+            </v-main>
+        </v-app>
+    </div>
+
     <x-slot:footerFiles>
-    </x-slot:footerFiles>    
+        <script>
+            new Vue({
+                el: '#app',
+                vuetify: new Vuetify(),
+                data: {},
+            })
+        </script>
+    </x-slot:footerFiles>
+
+
 </x-base-layout>
