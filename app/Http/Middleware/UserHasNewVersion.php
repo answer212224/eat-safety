b9d1d44871abe718c65da8bba8526074cc849805
Liam Li <liam.li@eatogether.com.tw>
2024-02-20 12:07:28 +0800

    Fix alert message formatting

--- app/Http/Middleware/UserHasNewVersion.php
+++ app/Http/Middleware/UserHasNewVersion.php
@@ -16,7 +16,7 @@ class UserHasNewVersion
      */
     public function handle(Request $request, Closure $next)
     {
-        alert()->info('請注意', '您正在使用舊版的食安巡檢系統，請儘速切換新版以獲得最佳的使用體驗。');
+        alert()->info("請注意", "您正在使用舊版的食安巡檢系統，請儘速切換新版以獲得最佳的使用體驗。");
         return $next($request);
     }
 }
