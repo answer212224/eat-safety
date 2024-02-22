aac44eccf1b79c4b61ee2603e366ef76f8147a78
Liam Li <liam.li@eatogether.com.tw>
2024-02-22 14:38:47 +0800

    Redirect to homepage instead of displaying an alert message

--- app/Http/Middleware/UserHasNewVersion.php
+++ app/Http/Middleware/UserHasNewVersion.php
@@ -16,7 +16,7 @@ class UserHasNewVersion
      */
     public function handle(Request $request, Closure $next)
     {
-        alert()->info("請注意", "您正在使用舊版的食安巡檢系統，請儘速切換新版以獲得最佳的使用體驗。點擊選單中的「稽核任務列表 v.2」即可切換至新版。");
-        return $next($request);
+
+        return redirect('/');
     }
 }
